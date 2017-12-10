91fd93b944fc87bbb0588940a3ba5ebdc9754d0e
Andric Tham <andric@nuisance.co>
2017-12-10 18:25:35 +0800

    Make calendar reducer

---
+++ src/reducers/index.js
@@ -0,0 +1,69 @@
+import { ADD_RECIPE, REMOVE_FROM_CALENDAR } from '../actions'
+
+// Here, we define the initial state of our application, which lets us decide how the shape of our state will look like.
+const initialCalendarState = {
+	sunday: {
+		breakfast: null,
+		lunch: null,
+		dinner: null,
+	},
+	monday: {
+		breakfast: null,
+		lunch: null,
+		dinner: null,
+	},
+	tuesday: {
+		breakfast: null,
+		lunch: null,
+		dinner: null,
+	},
+	wednesday: {
+		breakfast: null,
+		lunch: null,
+		dinner: null,
+	},
+	thursday: {
+		breakfast: null,
+		lunch: null,
+		dinner: null,
+	},
+	friday: {
+		breakfast: null,
+		lunch: null,
+		dinner: null,
+	},
+	saturday: {
+		breakfast: null,
+		lunch: null,
+		dinner: null,
+	},
+}
+
+const calendar = (state = initialCalendarState, action) => {
+	const { day, recipe, meal } = action
+
+	switch (action.type) {
+		case ADD_RECIPE:
+			return {
+				...state, // The same state that we had before
+				[day]: {
+					...state[day],
+					[meal]: recipe.label,
+				},
+				// Here, we’re using dynamic keys to find the specific day and meal (i.e., `wednesday` and `lunch`), to traverse the state object.
+			}
+		case REMOVE_FROM_CALENDAR:
+			return {
+				...state,
+				[day]: {
+					...state[day],
+					[meal]: null,
+				},
+			}
+		default:
+			return state
+	}
+}
+
+export default calendar
+
