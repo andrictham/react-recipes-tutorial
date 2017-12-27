ef24cdccda93bb0422318f523a941f9caf37f9a5
Andric Tham <andric@nuisance.co>
2017-12-27 17:58:57 +0800

    combineReducers in a root reducer

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,3 +1,5 @@
+import { combineReducers } from 'redux'
+
 import { ADD_RECIPE, REMOVE_FROM_CALENDAR } from '../actions'
 
 const food = (state = {}, action) => {
@@ -78,4 +80,7 @@ const calendar = (state = initialCalendarState, action) => {
 	}
 }
 
-export default calendar
+export default combineReducers({
+	food,
+	calendar,
+})
