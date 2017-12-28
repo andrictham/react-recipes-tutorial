124c20a9a62a1778789c4ba4c7005f4327c09cfc
Andric Tham <andric@nuisance.co>
2017-12-28 13:46:47 +0800

    Scaffold UI for adding/displaying meals

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,64 @@
 import React, { Component } from 'react'
 import { connect } from 'react-redux'
 import { addRecipe, removeFromCalendar } from '../actions'
+import { capitalize } from '../utils/helpers'
+import CalendarIcon from 'react-icons/lib/fa/calendar-plus-o'
 
 class App extends Component {
 	render() {
-		console.log(this.props)
-		return <div>Hello World</div>
+		const { calendar, remove } = this.props
+		const mealOrder = ['breakfast', 'lunch', 'dinner']
+		return (
+			<div className="container">
+				<ul className="meal-types">
+					{// Map over `mealOrder` array
+					// and print out the name of each meal as a header
+					mealOrder.map(mealType => (
+						<li key={mealType} className="subheader">
+							{capitalize(mealType)}
+						</li>
+					))}
+				</ul>
+
+				<div className="calendar">
+					<div className="days">
+						{// Map over `calendar` array created in mapStateToProps
+						// and print out the name of each day as a header
+						calendar.map(({ day }) => (
+							<h3 key={day} className="subheader">
+								{capitalize(day)}
+							</h3>
+						))}
+					</div>
+					<div className="icon-grid">
+						{// For each meal, in each day of the week:
+						calendar.map(({ day, meals }) => (
+							// Destructure `day` and `meals` from each object in the `calendar` array
+							<ul key={day}>
+								{mealOrder.map(meal => (
+									<li key={meal} className="meal">
+										{// If there is a specific meal added to that day, then show that meal
+										meals[meal] ? (
+											<div className="food-item">
+												<img src={meals[meal].image} alt={meals[meal].label} />
+												<button onClick={() => remove({ meal, day })}>
+													Clear
+												</button>
+											</div>
+										) : (
+											// If not, render a button that lets users add a meal
+											<button className="icon-btn">
+												<CalendarIcon size={30} />
+											</button>
+										)}
+									</li>
+								))}
+							</ul>
+						))}
+					</div>
+				</div>
+			</div>
+		)
 	}
 }
 
